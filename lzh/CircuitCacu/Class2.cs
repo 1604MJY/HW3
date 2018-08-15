f4de15da3bdc1d3111ad5ba730c538bae05f318e
Lester7LIU <1293767845@qq.com>
2018-08-15 11:19:45 +0800

    lzh's lei

---
+++ lzh/CircuitCacu/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitCacu
+{
+    public class Cacu2
+    {
+        static public double Cacu2(Circuit object)
+        {
+            double Resisnum, current;
+            Resisnum = (object.Resis1 * object.Resis3) / (object.Resis1 + object.Resis3);
+            current = Math.Round((object.Voltage / (object.Resis2 + Resisnum)) + (object.Voltage / object.Resis4),3);
+            return current;
+
+            
+        }
+    }
